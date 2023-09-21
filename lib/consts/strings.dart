905c01bc192cf0a6c1762b392121bce50d18bbb6
mabans <kamauian614@gmail.com>
2023-09-21 08:09:28 +0300

    Initial commit

---
+++ lib/consts/strings.dart
@@ -0,0 +1,9 @@
+const appName = "Shopie Seller App",
+    login = "Login",
+    welcome = "Hi, Welcome",
+    email = "Email",
+    password = "Password",
+    forgotPassword = "Forgot Password",
+    anyProblems = "Any problems contact Admin",
+
+credits ="@Mabans.com";
